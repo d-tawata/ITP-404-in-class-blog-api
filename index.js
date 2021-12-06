ebf3ff3d06cee8e58498a079420791bab9272a21
dpsalced <dpsalced@usc.edu>
2021-12-05 17:43:08 -0800

    added PUT and DELETE

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 require("dotenv").config();
 
-const express = require("express"); // CommonJS as opposed to import React from 'react' -> ES6 Modules
-const pg = require("pg");
+const { Client } = require("pg");
+const express = require("express");
 const cors = require("cors");
-
-const Client = pg.Client; // instead of const { Client } = require("pg");
 
 const app = express();
 
-app.use(express.json()); // middleware to deserialize JSON in request
-app.use(cors()); // middleware to allow cross-domain AJAX requests
+app.use(express.json());
+app.use(cors());
 
-app.get('/api/posts', (request, response) => {
+app.get("/api/posts", (request, response) => {
     const client = createClient();
 
     client.connect().then(() => {
-        client.query('SELECT * FROM posts').then((queryResponse) => {
+        client.query("SELECT * FROM posts").then((queryResponse) => {
             response.json(queryResponse.rows);
         });
     });
 });
 
-app.get('/api/posts/:id', (request, response) => {
+app.get("/api/posts/:id", (request, response) => {
     const client = createClient();
 
     client.connect().then(() => {
         client
-            .query('SELECT * FROM posts WHERE id = $1', [request.params.id])
+            .query("SELECT * FROM posts WHERE id = $1 LIMIT 1", [request.params.id])
             .then((queryResponse) => {
-                if (queryResponse.rows.length >= 1) {
+                if (queryResponse.rowCount >= 1) {
                     response.json(queryResponse.rows[0]);
                 } else {
                     response.status(404).send();
@@ -37,27 +35,75 @@ app.get('/api/posts/:id', (request, response) => {
     });
 });
 
-app.post('/api/posts', (request, response) => {
+app.post("/api/posts", (request, response) => {
     const client = createClient();
 
     client.connect().then(() => {
         client
             .query("INSERT INTO posts (title, body) VALUES ($1, $2) RETURNING *", [
                 request.body.title,
-                request.body.body
+                request.body.body,
             ])
             .then((queryResponse) => {
                 response.json(queryResponse.rows[0]);
             });
     });
-})
+});
+
+/*
+  Class exercise
+  Create DELETE /api/posts/:id
+  SQL: https://www.w3schools.com/sql/sql_delete.asp
+  Send a response with a 204 status code and an empty response body if rowCount = 1.
+  Otherwise, send a response with a 404 status code and an empty response body.
+*/
+app.delete("/api/posts/:id", (request, response) => {
+    const client = createClient();
+
+    client.connect().then(() => {
+        client
+            .query("DELETE FROM posts WHERE id = $1", [request.params.id])
+            .then((queryResponse) => {
+                if (queryResponse.rowCount === 1) {
+                    response.status(204).send();
+                } else {
+                    response.status(404).send();
+                }
+            });
+    });
+});
+
+/*
+  Class exercise
+  Create PUT /api/posts/:id
+  SQL: https://www.w3schools.com/sql/sql_update.asp
+  Send the updated post as JSON in the response body if rowCount = 1.
+  Be sure to include "RETURNING *" in your SQL like we did in POST /api/posts.
+  Otherwise, send a response with a 404 status code and an empty response body.
+*/
+app.put("/api/posts/:id", (request, response) => {
+    const client = createClient();
+
+    client.connect().then(() => {
+        client
+            .query(
+                "UPDATE posts SET title = $1, body = $2 WHERE id = $3 RETURNING *",
+                [request.body.title, request.body.body, request.params.id]
+            )
+            .then((queryResponse) => {
+                if (queryResponse.rowCount === 1) {
+                    response.json(queryResponse.rows[0]);
+                } else {
+                    response.status(404).send();
+                }
+            });
+    });
+});
 
 function createClient() {
     const client = new Client({
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        }
+        ssl: { rejectUnauthorized: false },
     });
 
     return client;
